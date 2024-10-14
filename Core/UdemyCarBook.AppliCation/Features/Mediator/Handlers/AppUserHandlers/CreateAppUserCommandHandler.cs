5eede6b64451cc75678dfd47b8bfd8d8025ebec0
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-10-14 16:01:05 +0300

    register mediator and enum added

---
+++ Core/UdemyCarBook.AppliCation/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.AppliCation.Enums;
+using UdemyCarBook.AppliCation.Features.Mediator.Commands.AppUserCommands;
+using UdemyCarBook.AppliCation.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.AppliCation.Features.Mediator.Handlers.AppUserHandlers
+{
+    public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+    {
+        private readonly IRepository<AppUser> _repository;
+
+        public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+        {
+             await _repository.CreateAsync(new AppUser
+            {
+                Password = request.Password,
+                Username = request.Username,
+                AppRoleId=(int)RolesType.Member
+            });
+        }
+    }
+}
